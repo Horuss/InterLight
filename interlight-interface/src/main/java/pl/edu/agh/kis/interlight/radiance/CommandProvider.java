e010b0aab2593d2b6ef113a1a0ec72f6d01586f8
Jakub Czajkowski <jakub.czajkowski1@gmail.com>
2015-02-15 00:37:56 +0100

    command provider

---
+++ interlight-interface/src/main/java/pl/edu/agh/kis/interlight/radiance/CommandProvider.java
@@ -0,0 +1,38 @@
+package pl.edu.agh.kis.interlight.radiance;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class CommandProvider {
+
+	private String systemDir;
+
+	public CommandProvider() {
+		this.systemDir = createSystemDir();
+	}
+
+	public String getCommandsPath() {
+		return Paths.get("lib/radiance/" + systemDir + "/")
+				.toAbsolutePath().toString();
+	}
+
+	public String createCommand(String command) {
+		return getCommandsPath() + File.separator + command;
+	}
+
+	private String createSystemDir() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		if (osName.contains("win")) {
+			return "win";
+		} else if (osName.contains("linux")) {
+			if (System.getProperty("os.arch").contains("64")) {
+				return "linux64";
+			} else {
+				return "linux32";
+			}
+		} else {
+			throw new UnsupportedOperationException("Unsupported OS");
+		}
+	}
+
+}
---
+++ interlight-interface/src/test/java/pl/edu/agh/kis/interlight/radiance/CommandProviderTest.java
@@ -0,0 +1,61 @@
+package pl.edu.agh.kis.interlight.radiance;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CommandProviderTest {
+	
+	private static String osName;
+	
+	@BeforeClass
+	public static void setUpClass() {
+		CommandProviderTest.osName = System.getProperty("os.name");
+	}
+	
+	@Before
+	public void setUp() {
+		System.setProperty("os.name", CommandProviderTest.osName);
+	}
+	
+	@Test
+	public void getCommandsPathTest() {
+		// given
+		CommandProvider commandProvider = new CommandProvider();
+		
+		// when
+		String s = commandProvider.getCommandsPath();
+		
+		// then
+		Assert.assertTrue(s.contains("lib" + File.separator + "radiance"));
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void unsupportedOsTest() {
+		// given
+		System.setProperty("os.name", "macOS");
+		CommandProvider commandProvider = new CommandProvider();
+		
+		// when
+		commandProvider.getCommandsPath();
+		
+		// then throws exception
+		System.clearProperty("os.name");
+	}
+	
+	@Test
+	public void createCommandTest() {
+		// given
+		CommandProvider commandProvider = new CommandProvider();
+		
+		// when
+		String s = commandProvider.createCommand("somecommand");
+		
+		// then
+		Assert.assertTrue(s.endsWith(File.separator + "somecommand"));
+	}
+	
+}
